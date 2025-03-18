0b5559dfcbd0d8b305563e6bd289d1e17adcd922
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-03-18 13:17:07 +0700

    penambahan do

--- file2.cpp
+++ file2.cpp
@@ -13,3 +13,11 @@ int main(){
     int pilihan;
     float panjang, lebar;
     float jariJari;
+    do{
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;}
